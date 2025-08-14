a671001ff5f0fe077cd45f116a8cfa348cb26031
jfmarkan <jfmarkan@gmail.com>
2025-08-14 10:56:18 +0200

    Created seeders to add Teams and populating those teams with members.

---
+++ backend/database/seeders/TeamMemberSeeder.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\TeamMember;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Illuminate\support\Carbon;
+
+class TeamMemberSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $teamMembers = [
+            [
+                'team_id' => 1,
+                'user_id' => 4,
+                'role' => 'MANAGER',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+            [
+                'team_id' => 2,
+                'user_id' => 5,
+                'role' => 'MANAGER',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+            [
+                'team_id' => 3,
+                'user_id' => 6,
+                'role' => 'MANAGER',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+            [
+                'team_id' => 1,
+                'user_id' => 7,
+                'role' => 'SALES_REP',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+            [
+                'team_id' => 1,
+                'user_id' => 8,
+                'role' => 'SALES_REP',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+            [
+                'team_id' => 1,
+                'user_id' => 9,
+                'role' => 'SALES_REP',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+            [
+                'team_id' => 1,
+                'user_id' => 12,
+                'role' => 'SALES_REP',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+            [
+                'team_id' => 1,
+                'user_id' => 15,
+                'role' => 'SALES_REP',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+            [
+                'team_id' => 1,
+                'user_id' => 18,
+                'role' => 'SALES_REP',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+            [
+                'team_id' => 2,
+                'user_id' => 7,
+                'role' => 'SALES_REP',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+            [
+                'team_id' => 2,
+                'user_id' => 10,
+                'role' => 'SALES_REP',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+            [
+                'team_id' => 2,
+                'user_id' => 11,
+                'role' => 'SALES_REP',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+            [
+                'team_id' => 2,
+                'user_id' => 12,
+                'role' => 'SALES_REP',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+            [
+                'team_id' => 2,
+                'user_id' => 13,
+                'role' => 'SALES_REP',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+            [
+                'team_id' => 2,
+                'user_id' => 14,
+                'role' => 'SALES_REP',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+            [
+                'team_id' => 2,
+                'user_id' => 16,
+                'role' => 'SALES_REP',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+            [
+                'team_id' => 2,
+                'user_id' => 17,
+                'role' => 'SALES_REP',
+                'created_at' => Now(),
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ],
+        ];
+
+        foreach ($teamMembers as $teamMember) {
+            $newTeamMember = new TeamMember();
+            $newTeamMember->team_id = $teamMember['team_id'];
+            $newTeamMember->user_id = $teamMember['user_id'];
+            $newTeamMember->role = $teamMember['role'];
+            $newTeamMember->created_at = $teamMember['created_at'];
+            $newTeamMember->updated_at = $teamMember['team_id'];
+            $newTeamMember->deleted_at = $teamMember['deleted_at'];
+            $newTeamMember->save();
+        };
+    }
+}
