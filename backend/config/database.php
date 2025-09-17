9d1da5a5001888d3dd426856a2fe2e0302671f73
Markan-Juan-Francisco <markan.juanfrancisco@steinbacher.at>
2025-09-17 10:18:46 +0200

    Fix: Added connection to SQL Server as read_only

--- backend/config/database.php
+++ backend/config/database.php
@@ -97,22 +97,18 @@ return [
             'sslmode' => 'prefer',
         ],
 
-        'sqlsrv' => [
+        'sqlsrv_stbspot' => [
             'driver' => 'sqlsrv',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
+            'host' => env('MSSQL_HOST', 'localhost'),
+            'port' => env('MSSQL_PORT', '1433'),
+            'database' => env('MSSQL_DATABASE', 'forge'),
+            'username' => env('MSSQL_USERNAME', 'forge'),
+            'password' => env('MSSQL_PASSWORD', ''),
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
-    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -171,4 +167,5 @@ return [
 
     ],
 
+]
 ];
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Auth\UserInvitationController;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +19,12 @@ Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/invite-user', [UserInvitationController::class, 'invite']);
 Route::get('/invitation/{token}', [UserInvitationController::class, 'validateToken']);
 Route::post('/finish-registration/{token}', [UserInvitationController::class, 'completeRegistration']);
+
+Route::get('/mssql-test', function () {
+    try {
+        DB::connection('sqlsrv_stbspot')->getPdo();
+        return '✅ Conectado a SQL Server (sqlsrv)';
+    } catch (\Throwable $e) {
+        return '❌ Error: ' . $e->getMessage();
+    }
+});
